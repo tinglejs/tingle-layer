ecd9b7937871122faf45fecf1f260d3722e79595
拂山 <jiasong.js@alibaba-inc.com>
2015-06-07 21:35:54 +0800

    init

---
+++ src/Layer.js
@@ -0,0 +1,28 @@
+/**
+ * Layer Component for tinglejs
+ * @auther gnosaij
+ *
+ * Copyright 2014-2015, Tingle Team, Alinw.
+ * All rights reserved.
+ */
+class Layer extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>tingle-layer component</div>
+        );
+    }
+}
+
+Layer.defaultProps = {
+}
+
+// http://facebook.github.io/react/docs/reusable-components.html
+Layer.propTypes = {
+}
+
+module.exports = Layer;
