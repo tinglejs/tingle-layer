92e98a19c8a212e8430835572dd63ce486da020e
拂山 <jiasong.js@alibaba-inc.com>
2015-07-08 23:58:40 +0800

    update

--- demo/LayerDemo.js
+++ demo/LayerDemo.js
@@ -1,12 +1,5 @@
-// TODO: move the two lines below to tingle-context
-require("fastclick").attach(document.body);
-React.initializeTouchEvents(true);
-
 
 var Layer = require('../src');
-
-// TODO: move the line to tingle-env
-React.initializeTouchEvents(true);
 
 class Demo extends React.Component {
 
